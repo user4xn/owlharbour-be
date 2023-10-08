16f1cc02187c8ed56cb6b40dae5162bd5611eb35
candrabudi <candrasachio@gmail.com>
2023-10-08 14:27:17 +0700

    create api get setting web

--- internal/app/setting/router.go
+++ internal/app/setting/router.go
@@ -8,8 +8,9 @@ import (
 
 // This function accepts gin.Routergroup to define a group route
 func (h *handler) Router(g *gin.RouterGroup) {
-	g.GET("/", h.GetDataSetting)
+	g.GET("/mobile", h.GetDataSetting)
 
 	g.Use(middleware.Authenticate())
+	g.GET("/web", h.GetDataSettingWeb)
 	g.POST("/create-or-update", h.Store)
 }
--- internal/dto/appsetting.go
+++ internal/dto/appsetting.go
@@ -11,6 +11,22 @@ type (
 		ApkDownloadLink string `json:"apk_min_download"`
 	}
 
+	GetDataSettingWeb struct {
+		HarbourCode     int           `json:"harbour_code"`
+		HarbourName     string        `json:"harbour_name"`
+		Mode            string        `json:"mode"`
+		ApkMinVersion   string        `json:"apk_min_version"`
+		Interval        int           `json:"interval"`
+		Range           int           `json:"range"`
+		ApkDownloadLink string        `json:"apk_min_download"`
+		Geofances       []GetGeofance `json:"geofances"`
+	}
+
+	GetGeofance struct {
+		Long string `json:"long"`
+		Lat  string `json:"lat"`
+	}
+
 	PayloadStoreSetting struct {
 		HarbourCode     int                  `json:"harbour_code" binding:"required"`
 		HarbourName     string               `json:"harbour_name" binding:"required"`
