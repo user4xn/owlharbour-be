8fa285254d8c6baabf2f801db634fb0440049e0f
candrabudi <candrasachio@gmail.com>
2023-10-11 21:58:20 +0700

    create api ship location report

--- internal/app/report/service.go
+++ internal/app/report/service.go
@@ -13,6 +13,7 @@ type service struct {
 
 type Service interface {
 	ShipDocking(ctx context.Context, request dto.ReportShipDockedParam) ([]dto.ReportShipDockingResponse, error)
+	ShipLocation(ctx context.Context, request dto.ReportShipLocationParam) ([]dto.ReportShipLocationResponse, error)
 }
 
 func NewService(f *factory.Factory) Service {
@@ -23,6 +24,15 @@ func NewService(f *factory.Factory) Service {
 
 func (s *service) ShipDocking(ctx context.Context, request dto.ReportShipDockedParam) ([]dto.ReportShipDockingResponse, error) {
 	res, err := s.shipRepository.ReportShipDocking(ctx, request)
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
+}
+
+func (s *service) ShipLocation(ctx context.Context, request dto.ReportShipLocationParam) ([]dto.ReportShipLocationResponse, error) {
+	res, err := s.shipRepository.ReportShipLocation(ctx, request)
 	if err != nil {
 		return nil, err
 	}
