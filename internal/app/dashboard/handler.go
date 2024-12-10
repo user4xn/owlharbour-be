9135b492e63fd872515aa90f6fa8a5aa33d6ebfb
Wildan Candra <wildan.desperado@gmail.com>
2024-12-10 13:16:06 +0700

    Websocket Secured

--- internal/app/dashboard/handler.go
+++ internal/app/dashboard/handler.go
@@ -47,6 +47,14 @@ func (h *handler) LastestDockedShip(c *gin.Context) {
 
 func (h *handler) ShipMonitorWebsocket(c *gin.Context) {
 	ctx := c.Request.Context()
+
+	token := c.Query("token")
+	if token == "" || token != util.GetEnv("WEBSOCKET_API_KEY", "fallback") {
+		response := util.APIResponse("token is not valid", http.StatusBadRequest, "failed", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
 	conn, err := upgrader.Upgrade(c.Writer, c.Request, nil)
 	if err != nil {
 		log.Logging("Failed on ws handshake: %s", err.Error()).Error()
@@ -87,12 +95,12 @@ func (h *handler) ShipMonitorWebsocket(c *gin.Context) {
 		}
 
 		// Calculate the number of batches required
-		numBatches := (int(totalShips) + batchSize - 1) / batchSize
+		numBatches := (int(totalShips) + batchSize - 1) / batchSize //100 + 10 - 1 = 109/10 = 10,9
 
-		for batch := 1; batch <= numBatches; batch++ {
-			start := (batch - 1) * batchSize
-			end := start + batchSize
-			if end > int(totalShips) {
+		for batch := 1; batch <= numBatches; batch++ { // 1 2 3 4 5 6 7 8 9 10
+			start := (batch - 1) * batchSize // offset ships (1 - 1 = 0 * 10 = 0), first offset 0
+			end := start + batchSize         // offset end ships data (0 + 10) = 10
+			if end > int(totalShips) {       // if 10 > 100 = end = 110
 				end = int(totalShips)
 			}
 			log.Logging("Fetching ships | %d | %d |", start, end).Info()
@@ -121,11 +129,11 @@ func (h *handler) ShipMonitorWebsocket(c *gin.Context) {
 				continue
 			}
 
-			time.Sleep(rate)
+			time.Sleep(rate) // rate sleep interval
 
-			if start+batchSize >= int(totalShips) {
-				rate = 10 * time.Second
-				batch = 0
+			if start+batchSize >= int(totalShips) { // 0+10 >= 110. if the rest all ship are sent
+				rate = 10 * time.Second //lower the rate sleep interval
+				batch = 0               //batch set to 0
 			}
 		}
 	}
--- internal/app/dashboard/router.go
+++ internal/app/dashboard/router.go
@@ -8,9 +8,8 @@ import (
 
 // This function accepts gin.Routergroup to define a group route
 func (h *handler) Router(g *gin.RouterGroup) {
-	g.GET("/ship-monitor/open-websocket", h.ShipMonitorWebsocket)
-	g.Use(middleware.Authenticate())
 	g.GET("/ship-monitor/websocket", h.ShipMonitorWebsocket)
+	g.Use(middleware.Authenticate())
 	g.GET("/statistic", h.HarbourStatistic)
 	g.GET("/terrain-chart", h.TerrainChart)
 	g.GET("/logs-chart", h.LogsChart)
